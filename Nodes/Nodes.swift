dcd7a49e31fca873a8fb02c04c8e2fbfa5449c26
Kasper Welner <kawe@nodes.dk>
2016-03-18 15:57:35 +0100

    Initial commit

---
+++ Nodes/Nodes.swift
@@ -0,0 +1,102 @@
+//
+//  Nodes.swift
+//  Nodes
+//
+//  Created by Kasper Welner on 18/03/16.
+//  Copyright © 2016 Nodes. All rights reserved.
+//
+
+import Foundation
+import Blobfish
+import Serializable
+
+public struct BlobfishConfiguration {
+    static func errorCodeMapping() -> [Int : Blobfish.AlamofireConfig.ErrorCategory] {
+        return [
+            441 : .Token,
+            442 : .Token,
+            443 : .Token,
+        ]
+    }
+}
+
+public func unwrapper() -> Parser.Unwrapper  {
+    return { (sourceDictionary, type) in
+        if let nestedObject: AnyObject = sourceDictionary["data"] {
+            return nestedObject
+        }
+        
+        if let nestedObject: AnyObject = sourceDictionary[String(type.dynamicType)] {
+            return nestedObject
+        }
+        
+        return sourceDictionary
+    }
+}
+
+struct Pagination {
+    var total = 0
+    var count = 0
+    var perPage = 0
+    var currentPage = 0
+    var totalPages = 0
+}
+
+extension Pagination: Serializable {
+    init(dictionary: NSDictionary?) {
+        total       <== (self, dictionary, "total")
+        count       <== (self, dictionary, "count")
+        perPage     <== (self, dictionary, "per_page")
+        currentPage <== (self, dictionary, "current_page")
+        totalPages  <== (self, dictionary, "total_pages")
+    }
+    
+    func encodableRepresentation() -> NSCoding {
+        let dict = NSMutableDictionary()
+        (dict, "total")        <== total
+        (dict, "count")        <== count
+        (dict, "per_page")     <== perPage
+        (dict, "current_page") <== currentPage
+        (dict, "total_pages")  <== totalPages
+        return dict
+    }
+}
+
+struct Meta {
+    var pagination = Pagination()
+}
+
+extension Meta: Serializable {
+    init(dictionary: NSDictionary?) {
+        pagination <== (self, dictionary, "pagination")
+    }
+    
+    func encodableRepresentation() -> NSCoding {
+        let dict = NSMutableDictionary()
+        (dict, "pagination") <== pagination
+        return dict
+    }
+}
+protocol PaginatedResponseProtocol { }
+
+final class PaginatedResponse<T:Serializable> {
+    var data:[T] = [T]()
+    var meta:Meta = Meta()
+}
+
+extension PaginatedResponse: PaginatedResponseProtocol {}
+
+extension PaginatedResponse: Serializable {
+    convenience init(dictionary: NSDictionary?) {
+        self.init()
+        data <== (self, dictionary, "data")
+        meta <== (self, dictionary, "meta")
+    }
+    
+    func encodableRepresentation() -> NSCoding {
+        let dict = NSMutableDictionary()
+        (dict, "data") <== data
+        (dict, "meta") <== meta
+        return dict
+    }
+}
