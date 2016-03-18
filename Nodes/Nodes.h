27aef337a33d65e4a53dedf0872f8dfa6757cfa6
Kasper Welner <kawe@nodes.dk>
2016-03-18 11:39:48 +0100

    Initial Commit

---
+++ Nodes/Nodes.h
@@ -0,0 +1,19 @@
+//
+//  Nodes.h
+//  Nodes
+//
+//  Created by Kasper Welner on 18/03/16.
+//  Copyright © 2016 Nodes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Nodes.
+FOUNDATION_EXPORT double NodesVersionNumber;
+
+//! Project version string for Nodes.
+FOUNDATION_EXPORT const unsigned char NodesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Nodes/PublicHeader.h>
+
+
