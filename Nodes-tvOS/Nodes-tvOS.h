0c32de5d369ba0b7f5ffa8be9fc069d3c0e7fb54
Todor <tobr@nodesagency.com>
2016-09-30 17:37:54 +0100

    Add tvOS target

---
+++ Nodes-tvOS/Nodes-tvOS.h
@@ -0,0 +1,19 @@
+//
+//  Nodes-tvOS.h
+//  Nodes-tvOS
+//
+//  Created by Todor Brachkov on 30/09/2016.
+//  Copyright © 2016 Nodes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Nodes-tvOS.
+FOUNDATION_EXPORT double Nodes_tvOSVersionNumber;
+
+//! Project version string for Nodes-tvOS.
+FOUNDATION_EXPORT const unsigned char Nodes_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Nodes_tvOS/PublicHeader.h>
+
+
--- Nodes/NodesUserAgent.swift
+++ Nodes/NodesUserAgent.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 import Alamofire
 
 public func userAgentString(isLiveApp:Bool? = nil) -> String {
